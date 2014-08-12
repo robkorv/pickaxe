c4c873d38db8200ee40b0adf0d6e5e5f32797d0d
Robbert Korving <r.korving@gmail.com>
2014-08-12 16:35:16 +0200

    good night!

---
+++ goodnight.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+def say_goodnight(name)
+  'Good night, ' + name
+end
+
+puts(say_goodnight('Robbert'))
+puts(say_goodnight('World'))
--- myprog.rb
+++ myprog.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-puts 'Hello, Ruby Programmer'
-puts "It's now #{Time.now}"
+puts('Hello, Ruby Programmer')
+puts("It's now #{Time.now}")
