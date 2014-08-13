f1f9e1ca23fc21821b6bc8d458a7b7d47e293821
Robbert Korving <r.korving@gmail.com>
2014-08-13 12:46:38 +0200

    string interpolation

--- goodnight.rb
+++ goodnight.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 def say_goodnight(name)
-  'Good night, ' + name
+  "Good night, #{name.capitalize}"
 end
 
 puts(say_goodnight('Robbert'))
 puts(say_goodnight('World'))
+puts(say_goodnight('robbert korving'))
